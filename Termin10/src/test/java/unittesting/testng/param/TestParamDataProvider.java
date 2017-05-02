4a0cacc62e94bf09b28fa26df538f05337444ea2
Aleksandra Pavkovic <aleksandra_p@live.com>
2017-05-02 11:42:06 +0200

    Dodavanje novog projekta

---
+++ Termin10/src/test/java/unittesting/testng/param/TestParamDataProvider.java
@@ -0,0 +1,24 @@
+package unittesting.testng.param;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestParamDataProvider {
+
+	@Test(dataProvider = "provideText")
+	public void test(String initialText, String expected) {
+		Assert.assertEquals(initialText + " with this!", expected);
+	}
+
+	@DataProvider(name = "provideText")
+	public Object[][] provideData() {
+
+		return new Object[][] { 
+			{ "salt", "salt with this!" }, 
+			{ "pork", "pork with this!" }, 
+			{ "cabbage", "cabbage with this!" }
+		};
+	}
+
+}
